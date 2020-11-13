11ec201ab20336e2d52f8cbd54d5f59bcd3b4a7f
fredeMi <frederique.michelix@gmail.com>
2020-11-13 14:44:27 +0100

    little improve front responsive card

--- application/views/front/home.php
+++ application/views/front/home.php
@@ -1,6 +1,6 @@
-<div class="row bg-secondary text-white">
+<div class="row mx-0 bg-secondary text-white">
     <div class="col py-4">
-        <h1>Fit Menu</h1>
+    <a href="http://fitmenu.frededev.fr/" class="btn btn-lg btn-outline-light"> Fit Menu </a>
     </div>
     <div class="col py-4 px-4">
         <a href="login" type="button" class="btn btn-info text-light">Se connecter</a>
@@ -9,9 +9,11 @@
         <a href="signin" type="button" class="btn btn-info text-light">S'inscrire</a>
     </div>
 </div>
-<div class="row text-center text-info">
+<div class="row mx-0 text-center text-info">
     <p class=" display-4">Votre carte de restaurant sans contact</p>
 </div>
-<div class="row">
-    <p>Fitmenu vous permet de créer une carte en ligne personnalisée pour chacun de vos établissements</p>
+<div class="row mx-0 bg-light text-dark p-4">
+    <p>Fitmenu vous permet de créer une carte en ligne personnalisée pour chacun de vos établissements.</p>
+    <p>Vous pouvez vous inscrire et gérer, personnaliser votre ou vos carte.s en ligne. Vos clients n'auront ensuite qu'à suivre le lien personnalisé de votre carte, pour faire leur choix dans le respect des gestes barrières. Votre carte en ligne est adaptée à l'affichage sur mobile, elle est désactivable si vous le souhaitez...</p>
+    <a href="http://fitmenu.frededev.fr/alacarte/resto3" class="btn btn-success" target="_blank">Carte de démonstration</a>
 </div>
--- application/views/front/login.php
+++ application/views/front/login.php
@@ -1,6 +1,6 @@
-<div class="row bg-secondary text-white">
+<div class="row mx-0 bg-secondary text-white">
     <div class="col py-4">
-        <h1>Fit Menu</h1>
+    <a href="http://fitmenu.frededev.fr/" class="btn btn-lg btn-outline-light"> Fit Menu </a>
     </div>
     <div class="col py-4 px-4">
         <a href="login" type="button" class="btn btn-info text-light">Se connecter</a>
@@ -10,7 +10,7 @@
     </div>
 </div>
 
-<div class="form-row my-3">
+<div class="form-row mx-0 mt-3 bg-light text-dark p-4">
     <div class="col">
     <h3 class="mb-4">Pour accéder à votre tableau de bord:</h3>
         <form method="post" action="login/checkUser">
